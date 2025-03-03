8a12f32ead2f1858e4b83015f64d23e38aab40e6
sinanylmaz <sinanylmaz07@gmail.com>
2025-03-03 22:55:50 +0300

    confix mask

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 BINANCE = dict(
-    api_key="bxOQuCRuslZjoNGQmiK7IRYIwce0l3cy65s0FJveSqrNPQUdJUOU82q6Nj3cZKH9",
-    api_secret="OlOwWFW9i2l3mbUxAchNH7h9jUTOWhaHB6BS7uRjB7OJpMecfaFlH00nVs0mnvxk",
+    api_key="*",
+    api_secret="*",
 )
 DATABASE_CONFIG = dict(
     database="postgres",
